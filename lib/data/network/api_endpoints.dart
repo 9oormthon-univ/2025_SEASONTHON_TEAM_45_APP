2955aed5cd390b98517528792a92da7b9da58e6a
hojung <jack777998@gmail.com>
2025-09-03 22:06:17 +0900

    aws 주소 변경 - 배포

--- lib/data/network/api_endpoints.dart
+++ lib/data/network/api_endpoints.dart
@@ -1,7 +1,7 @@
 class ApiEndpoints {
   // Base URL - 실제 서버 URL
-  // static const String baseUrl = 'http://13.209.99.158:8080'; - 배포용
-  static const String baseUrl = 'http://218.51.41.52:9600';
+  static const String baseUrl = 'http://13.209.99.158:8080';
+  //static const String baseUrl = 'http://218.51.41.52:9600';
   
   // Auth Endpoints
   static const String login = '/api/v1/auth/patient/sign-in';
